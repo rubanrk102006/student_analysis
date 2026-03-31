765b56195477bce881ffb41ee8a17dd8e71bfb25
rubanrk102006 <rubanrk102006@gmail.com>
2026-03-31 17:09:39 +0530

    Create student_analysis.py

---
+++ student_analysis.py
@@ -0,0 +1,9 @@
+marks = [85, 90, 78, 92, 88]
+
+avg = sum(marks) / len(marks)
+highest = max(marks)
+lowest = min(marks)
+
+print("Average:", avg)
+print("Highest:", highest)
+print("Lowest:", lowest)
